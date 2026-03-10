4f96882ece5332a19d2fee138a56b2af7c425497
himalraj977-jpg <himalraj977@gmail.com>
2026-03-10 16:39:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("wheel");
 const ctx = canvas.getContext("2d");
 
-const names = [
+const students = [
 "Alice",
 "Bob",
 "Charlie",
@@ -25,34 +25,32 @@ const radius = 200;
 
 function drawWheel(){
 
-let slice = 2*Math.PI / names.length;
+const slice = 2 * Math.PI / students.length;
 
-for(let i=0;i<names.length;i++){
+for(let i = 0; i < students.length; i++){
 
 let angle = i * slice;
 
 ctx.beginPath();
-
-ctx.moveTo(centerX,centerY);
+ctx.moveTo(centerX, centerY);
 
 ctx.fillStyle = colors[i];
 
-ctx.arc(centerX,centerY,radius,angle,angle+slice);
+ctx.arc(centerX, centerY, radius, angle, angle + slice);
 
 ctx.fill();
 
 ctx.save();
 
-ctx.translate(centerX,centerY);
+ctx.translate(centerX, centerY);
 ctx.rotate(angle + slice/2);
 
-ctx.fillStyle="white";
-ctx.font="16px Arial";
+ctx.fillStyle = "white";
+ctx.font = "16px Arial";
 
-ctx.fillText(names[i],100,10);
+ctx.fillText(students[i], 100, 10);
 
 ctx.restore();
-
 }
 
 }
@@ -61,9 +59,9 @@ drawWheel();
 
 function spin(){
 
-let random = Math.floor(Math.random()*names.length);
+let random = Math.floor(Math.random() * students.length);
 
 document.getElementById("result").innerHTML =
-"Selected Student: " + names[random];
+"Selected Student: " + students[random];
 
 }
